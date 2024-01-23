a32d6dac9dd690e77154f4740c00065a8cb77bcc
Daniel-k-dev <juyeon10120@naver.com>
2024-01-24 01:52:44 +0900

    test: 실패하는 테스트 작성

--- test/calculator/calculator.test.ts
+++ test/calculator/calculator.test.ts
@@ -2,7 +2,7 @@ import { Calculator } from "../../src/calculator/main"
 
 const calculator = new Calculator();
 
-test("테스트", () => {
+test("실패하는 테스트", () => {
     const actual = calculator.multiply(1, 2);
-    expect(actual).toBe(2)
+    expect(actual).toBe(5)
 })
